0f09f9a35281ef95e818d72696e517d447434b6f
Gabriel <loveragabriel20@gmail.com>
2023-03-13 01:08:24 -0300

    Images url fix

--- src/components/IconWhite.js
+++ src/components/IconWhite.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import Nueva from '../icons/imagotipo.svg'
 
 export const IconWhite = (props) => {
   return (
     <div>
         <img
-        src={'/imagotipo.svg'}
-        alt='hola'
-        loading="lazy"
+        src={Nueva}     
+        alt="Imagotipo Runar"
         width={'90px'}
         height={'90px'}
       />
