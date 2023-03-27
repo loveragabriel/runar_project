6b065d63a93d87637567dc5216eed14258353bb8
Gabriel <loveragabriel20@gmail.com>
2023-03-27 19:24:07 -0300

    ItemDetail Done

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import { Paper, Typography, Box } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { ItemCount } from './ItemCount';
+import { CircularProgress } from '@mui/material';
+
 
 const hoverCard = {
     padding: '1em',
@@ -16,35 +18,46 @@ const hoverCard = {
 
 function getItemsById(id) {
     const promise = new Promise((resolve) => {
-      setTimeout(() => {
-        const results = products.filter((product) => product.id === id);
-        resolve(results);
-      }, 1000);
+        setTimeout(() => {
+            const results = products.filter((product) => product.id === Number(id));
+            resolve(results);
+        }, 1000);
     });
-  
+
     return promise;
-  }
+}
 
 export const ItemDetailContainer = () => {
     const [product, setProduct] = useState({});
     let { idItem } = useParams();
+    const [loading, setLoading] = useState(true); // initialize loading state
+
 
     useEffect(() => {
-        getItemsById(idItem).then((results) => setProduct(results));
+        getItemsById(idItem).then((results) => {
+            if (results.length > 0) {
+                setProduct(results[0]);
+                setLoading(false);
+            }
+        });
     }, [idItem]);
 
     return (
-        <div>
-            <Paper elevation={5} sx={hoverCard}>
+        <div >
+
+            {loading ? ( // check loading status and render progress component if true
+                <CircularProgress sx={{ margin: '10em' }} />
+            ) : <Paper elevation={5} sx={hoverCard}>
                 <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
                     <Typography variant='h5'>{product.title}</Typography>
                 </Box>
                 <img src={product.img} alt='img' style={{ width: '250px' }} />
-                <Typography variant='h6'>{product['description']}</Typography>
+                <Typography variant='h6'>{product.description}</Typography>
                 <Typography variant='h6'>{product.category}</Typography>
                 <Typography variant='h6'>$ {product.price}</Typography>
                 <ItemCount></ItemCount>
             </Paper>
+            }
         </div>
     )
 }
